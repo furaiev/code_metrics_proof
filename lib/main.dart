d77cd1cce63ffb88051c9bbbd4fb3b03a728a100
Alex Furaiev <furaiev@gmail.com>
2022-05-18 10:42:47 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,6 @@ part 'part_of_test_class.dart';
 part 'part_of_test_class_2.dart';
 
 void main() {
-  _PartOfTestClassWithNumber();
   _PartOfTestClass();
+  _PartOfTestClassWithNumberInFileName();
 }
--- lib/part_of_test_class_2.dart
+++ lib/part_of_test_class_2.dart
@@ -1,5 +1,5 @@
 part of 'main.dart';
 
-class _PartOfTestClassWithNumber {
+class _PartOfTestClassWithNumberInFileName {
   String get testString => 'testString';
 }
