09764b5230867cd1f001c627d9dc0548150f0987
Alex Furaiev <furaiev@gmail.com>
2022-05-18 10:37:45 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'non_part_of_test_class.dart';
+import 'non_part_of_test_class_2.dart';
 
-part 'part_of_test_class.dart';
+part 'part_of_test_class_2.dart';
 
 void main() {
   _PartOfTestClass();
